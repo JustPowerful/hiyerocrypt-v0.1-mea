4ec036f7f1cee93ec35108d8951d5ee5b4543928
JustPowerful <50250163+JustPowerful@users.noreply.github.com>
2019-07-16 16:02:28 +0100

    Using Example

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import functions as fn
 
 # using the function
-enc = fn.encrypt("Hello World", 2)
+enc = fn.encrypt("Hello World")
 print("Encrypted :", enc)
-dec = fn.decrypt(enc, 2)
+dec = fn.decrypt(enc)
 print("Decrpyted :", dec)
