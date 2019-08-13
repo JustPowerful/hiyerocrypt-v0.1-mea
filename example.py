24c9c9f60f2cded35c9e6852c6dbd4880b9c2228
JustPowerful <50250163+JustPowerful@users.noreply.github.com>
2019-08-13 19:45:04 +0100

    example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from mea import LIST
 mea = MEA()
 lst = LIST()
 
-# returns a list of encrypted or decrypted ascii
+# returns a encrypted or a decrypted list
 # CAPS ARE NOT SUPPORTED (Bug)
 enclist = lst.enlist("hello world")
 declist = lst.delist("208 202 216 216 222 64 238 222 228 216 200") # don't forget to add the space between every ascii
